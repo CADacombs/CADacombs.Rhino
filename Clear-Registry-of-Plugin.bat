e7d5b9c5a18e4224a11bc57b59d26ecbc632fa32
spb-cad <gh@cadacombs.com>
2026-08-18 23:10:12 -0500

    Finalize 0.2.2: Strict continuity locking, bi-directional UI sync, and CLI overhaul

--- Clear-Registry-of-Plugin.bat
+++ Clear-Registry-of-Plugin.bat
@@ -3,10 +3,15 @@ echo =======================================
 echo Clearing CADacombs Registry Key...
 echo =======================================
 
+@echo on
+
 :: Use the native Windows Registry tool to forcefully delete the key
 reg delete "HKCU\Software\McNeel\Rhinoceros\8.0\Plug-ins\274aafc3-84b3-47a6-86af-dac682ff0c84" /f
+reg delete "HKCU\Software\McNeel\Rhinoceros\8.0\Global Options\Plug-ins\274aafc3-84b3-47a6-86af-dac682ff0c84" /f
+
+@echo off
 
 echo.
-echo Registry key cleared!
+:: echo Registry key cleared!
 echo.
 pause
